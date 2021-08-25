5c456d9e174b43ae4c9592be22bb93106ba327f1
JardelBrandon <jardel.brandon@academico.ifpb.edu.br>
2021-08-25 00:52:38 -0300

    Add project created
    Adding initialization of the project in Android Studio

---
+++ Twitter_API/app/src/main/java/br/com/jardel/twitter_api_cliente/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.jardel.twitter_api_cliente
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
